ae6d7c913d39014dcc0e58ae556ac85d85cd64c2
wsq <wangsq0926@163.com>
2021-08-24 10:38:12 +0800

    mathauill

--- src/app/pc/routes/mathquill/data.ts
+++ src/app/pc/routes/mathquill/data.ts
@@ -91,6 +91,161 @@ export const data = {
         </table><br>
       </div><br>
     </div>
-    <div><br></div>`,
+    <div><br></div>
+
+
+
+    1234567890asdfghjkl&nbsp;
+    <span id="insert-formula-f226fac3-aad4-829f-1e8c-4e61c6e680f3" contenteditable="false" class="">\bigtriangleup</span>&nbsp;
+    <div>常用公式</div>
+    <div>&nbsp;
+      <span id="insert-formula-7c4a24b9-b83f-8422-33bd-cf143291dc85" contenteditable="false" class="">\frac{1}{2}</span>&nbsp;&nbsp;
+      <span id="insert-formula-01d362ae-8c69-0b74-85a1-c40d6f21940a" contenteditable="false" class="">3^1/_2</span>&nbsp;&nbsp;
+      <span id="insert-formula-640a81a2-f13a-d18b-fb6a-7ddc8e2edee6" contenteditable="false" class="">x^2</span>&nbsp;&nbsp;
+      <span id="insert-formula-95d3afc9-4705-f206-a4a3-39d11d579ffa" contenteditable="false" class="">x_2</span>&nbsp;&nbsp;
+      <span id="insert-formula-d55922e2-215d-e307-367d-471d8522c8b7" contenteditable="false" class="">x_n^n22</span>&nbsp;&nbsp;
+      <span id="insert-formula-985079dd-d6ea-05fa-9b31-9e2af30f418f" contenteditable="false" class="">\overline{222}</span>&nbsp;&nbsp;
+      <span id="insert-formula-05129a7a-86dd-a3ff-1264-964d8be2e01c" contenteditable="false" class="">\sqrt{2}</span>&nbsp;&nbsp;
+      <span id="insert-formula-f85c8fcd-e400-de65-fde5-693969be883f" contenteditable="false" class="">\overline{\sqrt{2}}</span>&nbsp;&nbsp;
+      <span id="insert-formula-a924504a-acef-750c-1e6f-7cdd5615ed25" contenteditable="false" class="">\sqrt[3]{33}</span>&nbsp;&nbsp;
+      <span id="insert-formula-c267cda5-0a70-ae24-e4ed-7becd64f19df" contenteditable="false" class="">\sqrt[55]{666}</span>&nbsp;&nbsp;
+      <span id="insert-formula-67e52295-bb98-1cb7-0ba9-701c3134acf3" contenteditable="false" class="">\sum_{n=2}^2</span>&nbsp;&nbsp;
+      <span id="insert-formula-c01014a6-a309-c275-7159-51b8f2224daa" contenteditable="false" class="">\log_3\left(4\right)</span>&nbsp;&nbsp;
+      <span id="insert-formula-5956f268-eac8-4896-2946-d099c0ee20ca" contenteditable="false" class="">\ln\left(e\right)</span>&nbsp;
+      <br>
+    </div>
+    <div>符号</div>
+    <div>&nbsp;<span id="insert-formula-7782ac28-f4b6-57e6-a152-8b55c1ce89df" contenteditable="false" class="">+</span>&nbsp;&nbsp;
+      <span id="insert-formula-960802cb-4043-f6ed-4fb2-d186b9efe92c" contenteditable="false" class="">-</span>&nbsp;&nbsp;
+      <span id="insert-formula-dcdf253b-666e-dab0-d5a4-4085ac7c8370" contenteditable="false" class="">\pm</span>&nbsp;&nbsp;
+      <span id="insert-formula-827c7941-94a1-55bc-5938-2c737f9951fa" contenteditable="false" class="">\times</span>&nbsp;&nbsp;
+      <span id="insert-formula-2bd46013-5a8d-364a-36a7-34f7433d1b14" contenteditable="false" class="">\ast</span>&nbsp;&nbsp;
+      <span id="insert-formula-21888ac8-285d-ccb6-c163-673990c9fa5b" contenteditable="false" class="">\div</span>&nbsp;&nbsp;
+      <span id="insert-formula-efeb4c35-bdef-25f1-6f70-28d0cffdbde3" contenteditable="false" class="">/</span>&nbsp;&nbsp;
+      <span id="insert-formula-e0149b57-0870-bf56-4e08-23665dc75156" contenteditable="false" class="">\infty</span>&nbsp;&nbsp;
+      <span id="insert-formula-f246e97d-c2a3-7936-0473-10a0a0700e34" contenteditable="false" class="">\partial</span>&nbsp;&nbsp;
+      <span id="insert-formula-960a4286-ee10-2b4e-4394-d705f89e7f72" contenteditable="false" class="">=</span>&nbsp;&nbsp;
+      <span id="insert-formula-dab78eb6-4943-cc71-6918-30fbf218183f" contenteditable="false" class="">\bigtriangleup</span>&nbsp;&nbsp;
+      <span id="insert-formula-7fcfdc9a-c97b-e71a-3f13-bb47496a5ed7" contenteditable="false" class="">=</span>&nbsp;&nbsp;
+      <span id="insert-formula-0cbf9aaf-bf2d-95c4-26b0-5f08038677d8" contenteditable="false" class="">\ne</span>&nbsp;&nbsp;
+      <span id="insert-formula-159aa36f-00a1-827d-a0b7-aa1f62c957dc" contenteditable="false" class="">\approx</span>&nbsp;&nbsp;
+      <span id="insert-formula-de4ffe86-c772-8f82-7b27-5bf47488b4ff" contenteditable="false" class="">&gt;</span>&nbsp;&nbsp;
+      <span id="insert-formula-f265ac2b-dd3d-2f84-8cea-3697eb5905e1" contenteditable="false" class="">&lt;</span>&nbsp;&nbsp;
+      <span id="insert-formula-62193a58-8591-8af7-0aa9-56f8015870bc" contenteditable="false" class="">\ge</span>&nbsp;&nbsp;
+      <span id="insert-formula-4a2dec51-61da-6e95-b47e-62f109a8f976" contenteditable="false" class="">\le</span>&nbsp;&nbsp;
+      <span id="insert-formula-17ae5efc-2d8d-aa9f-38bc-8b4272262576" contenteditable="false" class="">\prod_2^1</span>&nbsp;&nbsp;
+      <span id="insert-formula-ad40c137-e73d-003a-4e3f-424668cd5ea8" contenteditable="false" class="">\int_x^0</span>&nbsp;&nbsp;
+      <span id="insert-formula-1ffac68f-d8e3-862c-940f-bc58fc2b2f11" contenteditable="false" class="">\rightarrow</span>&nbsp;
+      <span>
+        <i class="iconfont">
+          <span>
+            <i class="iconfont">
+              <span>
+                <i class="iconfont">
+                  <span>
+                    <i class="iconfont"></i>
+                  </span>
+                </i>
+              </span>
+            </i>
+          </span>
+        </i>
+      </span>
+      <br>
+    </div>
+    <div>
+      <span>
+        <i class="iconfont">
+          <span>
+            <i class="iconfont">
+              <span>
+                <i class="iconfont">
+                  <span><i class="iconfont">希腊字母</i></span>
+                </i>
+              </span>
+            </i>
+          </span>
+        </i>
+      </span>
+    </div>
+    <div>
+      <span>
+        <i class="iconfont">
+          <span>
+            <i class="iconfont">
+              <span>
+                <i class="iconfont">
+                  <span>
+                    <i class="iconfont">&nbsp;
+                      <span id="insert-formula-313db33b-e12f-b15d-1a2a-06046e42951b" contenteditable="false" class="">\alpha\beta\gamma\delta</span>&nbsp;&nbsp;
+                      <span id="insert-formula-7c41b9e5-5672-45bb-1327-3efa5d571a09" contenteditable="false" class="">\epsilon\phi</span>&nbsp;&nbsp;
+                      <span id="insert-formula-2fe13d4a-ba53-14fd-4da2-55a7c97568ea" contenteditable="false" class="">\lambda</span>&nbsp;&nbsp;
+                      <span id="insert-formula-26ac2955-c9f3-979e-ff0a-0dfab55dda2b" contenteditable="false" class="">\phi</span>&nbsp;&nbsp;
+                      <span id="insert-formula-bab3f7bb-4b20-79e1-1b8d-d07768e5baf4" contenteditable="false" class="">\Phi</span>&nbsp;&nbsp;
+                      <span id="insert-formula-bab9bed7-d65f-200c-76d4-94c3ea3b699e" contenteditable="false" class="">\mu</span>&nbsp;&nbsp;
+                      <span id="insert-formula-59f75996-86d8-938b-a1f3-b3373a11356f" contenteditable="false" class="">\rho</span>&nbsp;&nbsp;
+                      <span id="insert-formula-b8bdfa83-a8d0-8464-9738-c8e8efafedee" contenteditable="false" class="">\sigma</span>&nbsp;&nbsp;
+                      <span id="insert-formula-98f91d4f-06ca-9c87-3cb4-b1797049177d" contenteditable="false" class="">\omega</span>&nbsp;&nbsp;
+                      <span id="insert-formula-cd5556e6-8bc8-31cc-4220-eff3ace00c2a" contenteditable="false" class="">\\xi</span>&nbsp;&nbsp;
+                      <span id="insert-formula-7911dec7-06c2-bd09-5dbd-eb012249b83e" contenteditable="false" class="">\theta</span>&nbsp;&nbsp;
+                      <span id="insert-formula-9e9ca55c-c195-a02b-ba3b-2f60f60ed976" contenteditable="false" class="">\Sigma</span>&nbsp;&nbsp;
+                      <span id="insert-formula-7f6b1902-cb37-342d-727e-946bb551abf5" contenteditable="false" class="">\Gamma</span>&nbsp;<br>
+                    </i>
+                  </span>
+                </i>
+              </span>
+            </i>
+          </span>
+        </i>
+      </span>
+    </div>
+    <div>
+      <span>
+        <i class="iconfont">
+          <span>
+            <i class="iconfont">
+              <span>
+                <i class="iconfont">
+                  <span><i class="iconfont"><br></i></span>
+                </i>
+              </span>
+            </i>
+          </span>
+        </i>
+      </span>
+    </div>
+    <div>
+      <span>
+        <i class="iconfont">
+          <span>
+            <i class="iconfont">
+              <span>
+                <i class="iconfont">
+                  <span><i class="iconfont">全</i></span>
+                </i>
+              </span>
+            </i>
+          </span>
+        </i>
+      </span>
+    </div>
+    `,
 };
 
+// case 'rebar1':
+//   iconHtml = "<i class='iconfont'>&#xed21;</i>";
+//   break;
+// case 'rebar2':
+//   iconHtml = "<i class='iconfont'>&#xea1a;</i>";
+//   break;
+// case 'rebar3':
+//   iconHtml = "<i class='iconfont'>&#xed22;</i>";
+//   break;
+// case 'rebar4':
+//   iconHtml = "<i class='iconfont'>&#xed20;</i>";
+
+
+// 1234567890asdfghjkl&nbsp;<span id="insert-formula-f226fac3-aad4-829f-1e8c-4e61c6e680f3" contenteditable="false" class="">\bigtriangleup</span>&nbsp;<div>常用公式</div><div>&nbsp;<span id="insert-formula-7c4a24b9-b83f-8422-33bd-cf143291dc85" contenteditable="false" class="">\frac{1}{2}</span>&nbsp;&nbsp;<span id="insert-formula-01d362ae-8c69-0b74-85a1-c40d6f21940a" contenteditable="false" class="">3^1/_2</span>&nbsp;&nbsp;<span id="insert-formula-640a81a2-f13a-d18b-fb6a-7ddc8e2edee6" contenteditable="false" class="">x^2</span>&nbsp;&nbsp;<span id="insert-formula-95d3afc9-4705-f206-a4a3-39d11d579ffa" contenteditable="false" class="">x_2</span>&nbsp;&nbsp;<span id="insert-formula-d55922e2-215d-e307-367d-471d8522c8b7" contenteditable="false" class="">x_n^n22</span>&nbsp;&nbsp;<span id="insert-formula-985079dd-d6ea-05fa-9b31-9e2af30f418f" contenteditable="false" class="">\overline{222}</span>&nbsp;&nbsp;<span id="insert-formula-05129a7a-86dd-a3ff-1264-964d8be2e01c" contenteditable="false" class="">\sqrt{2}</span>&nbsp;&nbsp;<span id="insert-formula-f85c8fcd-e400-de65-fde5-693969be883f" contenteditable="false" class="">\overline{\sqrt{2}}</span>&nbsp;&nbsp;<span id="insert-formula-a924504a-acef-750c-1e6f-7cdd5615ed25" contenteditable="false" class="">\sqrt[3]{33}</span>&nbsp;&nbsp;<span id="insert-formula-c267cda5-0a70-ae24-e4ed-7becd64f19df" contenteditable="false" class="">\sqrt[55]{666}</span>&nbsp;&nbsp;<span id="insert-formula-67e52295-bb98-1cb7-0ba9-701c3134acf3" contenteditable="false" class="">\sum_{n=2}^2</span>&nbsp;&nbsp;<span id="insert-formula-c01014a6-a309-c275-7159-51b8f2224daa" contenteditable="false" class="">\log_3\left(4\right)</span>&nbsp;&nbsp;<span id="insert-formula-5956f268-eac8-4896-2946-d099c0ee20ca" contenteditable="false" class="">\ln\left(e\right)</span>&nbsp;<br></div><div>符号</div><div>&nbsp;<span id="insert-formula-7782ac28-f4b6-57e6-a152-8b55c1ce89df" contenteditable="false" class="">+</span>&nbsp;&nbsp;<span id="insert-formula-960802cb-4043-f6ed-4fb2-d186b9efe92c" contenteditable="false" class="">-</span>&nbsp;&nbsp;<span id="insert-formula-dcdf253b-666e-dab0-d5a4-4085ac7c8370" contenteditable="false" class="">\pm</span>&nbsp;&nbsp;<span id="insert-formula-827c7941-94a1-55bc-5938-2c737f9951fa" contenteditable="false" class="">\times</span>&nbsp;&nbsp;<span id="insert-formula-2bd46013-5a8d-364a-36a7-34f7433d1b14" contenteditable="false" class="">\ast</span>&nbsp;&nbsp;<span id="insert-formula-21888ac8-285d-ccb6-c163-673990c9fa5b" contenteditable="false" class="">\div</span>&nbsp;&nbsp;<span id="insert-formula-efeb4c35-bdef-25f1-6f70-28d0cffdbde3" contenteditable="false" class="">/</span>&nbsp;&nbsp;<span id="insert-formula-e0149b57-0870-bf56-4e08-23665dc75156" contenteditable="false" class="">\infty</span>&nbsp;&nbsp;<span id="insert-formula-f246e97d-c2a3-7936-0473-10a0a0700e34" contenteditable="false" class="">\partial</span>&nbsp;&nbsp;<span id="insert-formula-960a4286-ee10-2b4e-4394-d705f89e7f72" contenteditable="false" class="">=</span>&nbsp;&nbsp;<span id="insert-formula-dab78eb6-4943-cc71-6918-30fbf218183f" contenteditable="false" class="">\bigtriangleup</span>&nbsp;&nbsp;<span id="insert-formula-7fcfdc9a-c97b-e71a-3f13-bb47496a5ed7" contenteditable="false" class="">=</span>&nbsp;&nbsp;<span id="insert-formula-0cbf9aaf-bf2d-95c4-26b0-5f08038677d8" contenteditable="false" class="">\ne</span>&nbsp;&nbsp;<span id="insert-formula-159aa36f-00a1-827d-a0b7-aa1f62c957dc" contenteditable="false" class="">\approx</span>&nbsp;&nbsp;<span id="insert-formula-de4ffe86-c772-8f82-7b27-5bf47488b4ff" contenteditable="false" class="">&gt;</span>&nbsp;&nbsp;<span id="insert-formula-f265ac2b-dd3d-2f84-8cea-3697eb5905e1" contenteditable="false" class="">&lt;</span>&nbsp;&nbsp;<span id="insert-formula-62193a58-8591-8af7-0aa9-56f8015870bc" contenteditable="false" class="">\ge</span>&nbsp;&nbsp;<span id="insert-formula-4a2dec51-61da-6e95-b47e-62f109a8f976" contenteditable="false" class="">\le</span>&nbsp;&nbsp;<span id="insert-formula-17ae5efc-2d8d-aa9f-38bc-8b4272262576" contenteditable="false" class="">\prod_2^1</span>&nbsp;&nbsp;<span id="insert-formula-ad40c137-e73d-003a-4e3f-424668cd5ea8" contenteditable="false" class="">\int_x^0</span>&nbsp;&nbsp;<span id="insert-formula-1ffac68f-d8e3-862c-940f-bc58fc2b2f11" contenteditable="false" class="">\rightarrow</span>&nbsp;
+// <span><i class="iconfont">&#xed21;<span><i class="iconfont">&#xea1a;<span><i class="iconfont">&#xed22;<span><i class="iconfont">&#xed20;</i></span></i></span></i></span></i></span>
+// <br></div>
+// <div><span><i class="iconfont"><span><i class="iconfont"><span><i class="iconfont"><span><i class="iconfont">希腊字母</i></span></i></span></i></span></i></span></div><div><span><i class="iconfont"><span><i class="iconfont"><span><i class="iconfont"><span><i class="iconfont">&nbsp;<span id="insert-formula-313db33b-e12f-b15d-1a2a-06046e42951b" contenteditable="false" class="">\alpha\beta\gamma\delta</span>&nbsp;&nbsp;<span id="insert-formula-7c41b9e5-5672-45bb-1327-3efa5d571a09" contenteditable="false" class="">\epsilon\phi</span>&nbsp;&nbsp;<span id="insert-formula-2fe13d4a-ba53-14fd-4da2-55a7c97568ea" contenteditable="false" class="">\lambda</span>&nbsp;&nbsp;<span id="insert-formula-26ac2955-c9f3-979e-ff0a-0dfab55dda2b" contenteditable="false" class="">\phi</span>&nbsp;&nbsp;<span id="insert-formula-bab3f7bb-4b20-79e1-1b8d-d07768e5baf4" contenteditable="false" class="">\Phi</span>&nbsp;&nbsp;<span id="insert-formula-bab9bed7-d65f-200c-76d4-94c3ea3b699e" contenteditable="false" class="">\mu</span>&nbsp;&nbsp;<span id="insert-formula-59f75996-86d8-938b-a1f3-b3373a11356f" contenteditable="false" class="">\rho</span>&nbsp;&nbsp;<span id="insert-formula-b8bdfa83-a8d0-8464-9738-c8e8efafedee" contenteditable="false" class="">\sigma</span>&nbsp;&nbsp;<span id="insert-formula-98f91d4f-06ca-9c87-3cb4-b1797049177d" contenteditable="false" class="">\omega</span>&nbsp;&nbsp;<span id="insert-formula-cd5556e6-8bc8-31cc-4220-eff3ace00c2a" contenteditable="false" class="">\xi</span>&nbsp;&nbsp;<span id="insert-formula-7911dec7-06c2-bd09-5dbd-eb012249b83e" contenteditable="false" class="">\theta</span>&nbsp;&nbsp;<span id="insert-formula-9e9ca55c-c195-a02b-ba3b-2f60f60ed976" contenteditable="false" class="">\Sigma</span>&nbsp;&nbsp;<span id="insert-formula-7f6b1902-cb37-342d-727e-946bb551abf5" contenteditable="false" class="">\Gamma</span>&nbsp;<br></i></span></i></span></i></span></i></span></div><div><span><i class="iconfont"><span><i class="iconfont"><span><i class="iconfont"><span><i class="iconfont"><br></i></span></i></span></i></span></i></span></div><div><span><i class="iconfont"><span><i class="iconfont"><span><i class="iconfont"><span><i class="iconfont">全</i></span></i></span></i></span></i></span></div>
